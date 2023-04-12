eef8e7e5541705e3b10b68ef5806c77d8d4076b4
DuenoHfao <ngedric74@gmail.com>
2023-04-12 17:07:01 +0800

    added a function to find title of all the links of a certain subject

--- get_title.py
+++ get_title.py
@@ -7,9 +7,12 @@ def get_title(url):
     
     # using the BeautifulSoup module
     soup = BeautifulSoup(reqs.text, 'html.parser')
+    all_title =  soup.find_all('title')
+    print("All Title:", all_title)
+    for i in all_title:
+        i = str(i).strip()
+        print("i:", i)
+        findIndex = str(i)[7:len(i)-8]
+        print(findIndex)
     
-    # displaying the title
-    for title in soup.find_all('title'):
-        print(title.get_text())
-    
-    return title.get_text()
+    return all_title
--- main.py
+++ main.py
@@ -7,5 +7,14 @@ from get_title import *
 import pandas as pd
 
 user_input = input("Input subject name: ")
-input_subject(user_input)
+search_subject = input_subject(user_input)[0][0]
 
+dataset = pd.read_csv("Lecture_dB.csv")
+select_subj = dataset.loc[dataset["subject"] == search_subject]["link"]
+# print(dataset)
+# print(select_subj)
+temp_title = []
+for i in select_subj:
+    temp_title.append(get_title(i))
+    
+print(temp_title)
